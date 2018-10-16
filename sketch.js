da4c05131e6f8fcfc0ef4ad5a5a47f418d61d227
Ennio Sorrentino <enniosorretntino@gmail.com>
2018-10-16 22:39:54 +0200

    as05

--- sketch.js
+++ sketch.js
@@ -1,11 +1,54 @@
-function preload(){
-  // put preload code here
+var Obs =[]
+
+function setup(){
+
+  frameRate(50)
+  createCanvas(windowWidth,windowHeight)
+
+  var ObsNumber=width/10
+  for(i=0;i<ObsNumber;i++){ var newObj= new Obj()
+    Obs.push(newObj)
+  }
 }
 
-function setup() {
-  // put setup code here
+function draw(){
+
+
+      background(100)
+
+for(h=1;h<Obs.length;h++){
+
+ for(f=1;f<Obs.length;f++){
+  Obs[h].display= polygon( Obs[h].a,Obs[h].b,100,Obs[h].c )
+   Obs[h].a=h*100-100
+   Obs[h].b=f*10-100,100
+   Obs[h].c=(mouseX/100)%15
+
+   if(Obs[h].a+50>mouseX && Obs[h].a-50<mouseX)
+   {Obs[h].d= color(255-random(1,250),100,100+random(1,150))}
+   else{Obs[h].d= color(255-random(1,50),100+random(1,50),100)}
+
+  fill(Obs[h].d)
+}}
 }
 
-function draw() {
-  // put drawing code here
+function Obj() {
+  this.a=0
+  this.b= 0
+  this.ut=0
+  this.c=0
+  this.d= color(255-random(1,50),100+random(1,50),100)
+  this.display= 0
+}
+
+
+function polygon(x, y, radius, npoints) {
+  var angle = TWO_PI / npoints;
+  beginShape();
+  for (var a = 0; a < TWO_PI; a += angle) {
+    var sx = x + cos(a) * radius;
+    var sy = y + sin(a) * radius;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
